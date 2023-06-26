22ee1ef3471cd0c1aa4a2d091b56aa8934d01ae0
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-06-26 15:00:35 +0200

    Update room_ba.py

--- room_ba.py
+++ room_ba.py
@@ -2,9 +2,9 @@ import datetime
 import discord
 from discord.ext import tasks, commands
 from ics import Calendar, Event
-import asyncio
 import requests
 
+# Pour que le bot fonctionne, il faut renseigner votre token ici et l'url de votre fichier .ics (première ligne de la fonction get_the_right_room)
 TOKEN = "YOUR TOKEN HERE"
 
 intents = discord.Intents.all()
@@ -12,8 +12,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 #################################################
 
-def get_the_right_room(commande):
+# Fonction qui permet de lire la bonne partie du fichier .ics en fonction de la commande
+def get_the_right_room(commande: str) -> str:
+
     ics_url = "URL TO YOUR .ICS HERE"
+    # sends a GET request to the URL using the requests library. 
+    # The response from the server is stored in the response variable. 
+    # You can then access the response content using response.content
     response = requests.get(ics_url)
     today = datetime.date.today()
 
@@ -21,16 +26,21 @@ def get_the_right_room(commande):
         calendar = Calendar(response.text)
         events = [event for event in calendar.events if event.begin.date() == today]
         if events:
+            # Il y a deux événements par jour, le premier est le matin, le deuxième est l'après-midi
             event = events[0]
             description = event.description.encode('latin-1').decode('utf-8')
+            # Pas envie de récupérer toute la description, on récupère juste la ligne qui nous intéresse
             intervenant = ""
             for line in description.splitlines():
                 if line.startswith("- Intervenant(s) :"):
                     intervenant += line
+            # les fichiers .ics sont encodés en latin-1, on les décode en utf-8 pour pouvoir les afficher
             location = event.location.encode('latin-1').decode('utf-8')
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant}"
+            # On retourne le message à afficher formatté tout joli
+            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
         else:
             return "Aucun événement trouvé pour cette date."
+
 
     elif commande == "aprem":
         calendar = Calendar(response.text)
@@ -43,15 +53,17 @@ def get_the_right_room(commande):
                 if line.startswith("- Intervenant(s) :"):
                     intervenant += line
             location = event.location.encode('latin-1').decode('utf-8')
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant}"
+            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
         else:
             return "Aucun événement trouvé pour cette date."
         
+
     elif commande == "demain":
         tomorrow = datetime.date.today() + datetime.timedelta(days=1)
         calendar = Calendar(response.text)
         events = [event for event in calendar.events if event.begin.date() == tomorrow]
         if events:
+            # pour une raison que je ne comprends pas, ici, le seul moyen d'afficher le premier événement de la liste est de prendre le deuxième
             event = events[1]
             description = event.description.encode('latin-1').decode('utf-8')
             intervenant = ""
@@ -59,15 +71,16 @@ def get_the_right_room(commande):
                 if line.startswith("- Intervenant(s) :"):
                     intervenant += line
             location = event.location.encode('latin-1').decode('utf-8')
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant}"
+            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
         else:
             return "Aucun événement trouvé pour cette date."
         
     else:
-        return "Aucun événement trouvé pour cette date."
+        return "Wut?"
 
 #################################################
 
+# Action à effectuer pour la commande !matin
 @bot.command(name='matin', help="Affiche la salle de classe pour ce matin")
 async def room_command(ctx):    
     info = get_the_right_room("matin")
