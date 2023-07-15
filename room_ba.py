edebcd68d0b71bb737054c6f0c51ff0683b89858
redhox <morgan.c@neuf.fr>
2023-07-15 20:03:31 +0200

    merde

--- room_ba.py
+++ room_ba.py
@@ -19,7 +19,7 @@ intents = discord.Intents.all()
 intents.presences = False
 bot = commands.Bot(command_prefix='!', intents=intents)
 last_id_salon = {} #dicsionaire pour le salon automatique   
- 
+
 
 #################################################
 
@@ -83,7 +83,7 @@ def get_the_right_room(commande: str) -> str:
         return "Wut?"
 
 def get_the_comande(id):
-    return "id du salon update: ",id
+    return f"id du salon update! \n Prochain message automatique a {HEURE} "
 #################################################
 
 # Action à effectuer pour la commande !matin
@@ -107,77 +107,69 @@ async def room_tomorrow_command(ctx):
 @bot.command(name='id', help="Affiche la salle de classe pour le lendemain")
 async def command(ctx):    
     id_salon = ctx.channel.id
-    info = get_the_comande(id_salon)
+    info = get_the_comande(id_salon)#restour dans le salon que la comande est effective 
     print("salon id set : ",id_salon)
     last_id_salon['last_id'] = id_salon
-
     await ctx.send(info)
-    await on_ready(id_salon)
+    await on_ready()
 
 def automatique():
     response = requests.get(ICS_URL)
     today = datetime.date.today()
-    
     filtre_events = []
-    
+    tomorrow = datetime.date.today() + datetime.timedelta(days=-2)
     calendar = Calendar(response.text)
     events = [event for event in calendar.events if event.begin.date() == today]
+    
     if events:
-        # Il y a deux événements par jour, le premier [0] , le deuxième [1] 
-        event_matin = events[0]
-        event_aprem = events[1]
-        
-        description_matin = event_matin.description.encode('latin-1').decode('utf-8')
-        intervenant_matin = ""
-        for line in description_matin.splitlines():
-            if line.startswith("- Intervenant(s) :"):
-                intervenant_matin += line
-        location = event_matin.location.encode('latin-1').decode('utf-8')
-
-        description_aprem = event_aprem.description.encode('latin-1').decode('utf-8')
-        intervenant_aprem = ""
-        for line in description_aprem.splitlines():
-            if line.startswith("- Intervenant(s) :"):
-                intervenant_aprem += line
-        location = event_aprem.location.encode('latin-1').decode('utf-8')
-        
+        # Il y a deux événements par jour, le premier [0] , le deuxième [1] qui ont la posibilité de changer d'ordre
+        event_1 = events[0]
+        event_2 = events[1]
 #####################################################################################
 #determiné le matin de l'aprem par une liste de"filtre event"du plus tot au plus tard
 #####################################################################################
         for event in events:
             if event.begin.date() == today:
                 filtre_events.append(event)
-                
-        filtre_events.sort(key=lambda event: event.begin.time())
-        if filtre_events:
-            horaire_matin = filtre_events[0].begin.time()
-            horaire_aprem = filtre_events[1].begin.time()
+        if filtre_events.sort(key=lambda event: event.begin.time(0)) == event_1.begin.time():
+            event_matin = event_1
+            event_aprem = event_2
         else:
-            return "format de date non valide ou inexistant"
-
-        return f"matin : Salle : {location}Commence à : {event_matin.begin.time()}\n{intervenant_matin[2:]} \n\n aprem : Salle : {location}Commence à : {event_aprem.begin.time()}\n{intervenant_aprem[2:]} \n \n horaire matin:{horaire_matin} -- horaire aprem:{horaire_aprem}"
-
+            event_matin = event_2
+            event_aprem = event_1
+        return f"matin :\n {event_affichage(event_matin)}\n\n aprem :\n {event_affichage(event_aprem)}"
     else:
         return "Aucun événement trouvé pour cette date le matin."
 
+def event_affichage(event):
+    description = event.description.encode('latin-1').decode('utf-8')
+    intervenant = ""
+    for line in description.splitlines():
+        if line.startswith("- Intervenant(s) :"):
+            intervenant += line
+    location = event.location.encode('latin-1').decode('utf-8')
+    message = f" Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]} "
+    return message
 #################################################
-
-async def on_ready(id_salon):
-    print(f'We have logged in as {bot.user.name}')
+async def on_ready():
     if not hasattr(bot, 'is_schedule_running'):
         bot.is_schedule_running = True
-        schedule.every().day.at(HEURE).do(lambda: asyncio.create_task(send_message(id_salon)))  #asyncio.create_task() execute la coroutine en arrière-plan
+        schedule.every().day.at(HEURE).do(lambda: asyncio.create_task(send_message()))  #asyncio.create_task() execute la coroutine en arrière-plan
         # Run the schedule in the background
         while True:
             schedule.run_pending()
             await asyncio.sleep(1)
 
-async def send_message(id_salon):
+async def send_message():
     last_id = last_id_salon.get('last_id')
     channel = bot.get_channel(int(last_id))  # ID du channel room-bot
     if channel:
         await channel.send(automatique())
+
 print('Bot is ready.')
 print('heure prevu',HEURE)
 #asyncio.run(on_ready(ID_SALON))
+
+
 bot.run(TOKEN)
+print(f'We have logged in as {bot.user.name}')
