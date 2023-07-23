5a6457771e303259a6086fd74fc5b41f616d479a
coulm <morgan.coulm@gmail.com>
2023-07-23 15:25:06 +0200

    supresion de get the room

--- room_ba.py
+++ room_ba.py
@@ -22,87 +22,10 @@ last_id_salon = {} #dicsionaire pour le salon automatique
 
 
 #################################################
-
-def get_the_right_room(commande: str) -> str:
-    response = requests.get(ICS_URL)
-    today = datetime.date.today()
-    if commande == "matin":
-        calendar = Calendar(response.text)
-        events = [event for event in calendar.events if event.begin.date() == today]
-        if events:
-            # Il y a deux événements par jour, etrangement, étrangement, le premier [0] est celui de 14h00, le deuxième [1] est celui de 09h00
-            event = events[1]
-            description = event.description.encode('latin-1').decode('utf-8')
-            # Pas envie de récupérer toute la description, on récupère juste la ligne qui nous intéresse
-            intervenant = ""
-            for line in description.splitlines():
-                if line.startswith("- Intervenant(s) :"):
-                    intervenant += line
-            # les fichiers .ics sont encodés en latin-1, on les décode en utf-8 pour pouvoir les afficher
-            location = event.location.encode('latin-1').decode('utf-8')
-            # On retourne le message à afficher formatté tout joli
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
-        else:
-            return "Aucun événement trouvé pour cette date le matin."
-
-
-    elif commande == "aprem":
-        calendar = Calendar(response.text)
-        events = [event for event in calendar.events if event.begin.date() == today]
-        if events:
-            event = events[0]
-            description = event.description.encode('latin-1').decode('utf-8')
-            intervenant = ""
-            for line in description.splitlines():
-                if line.startswith("- Intervenant(s) :"):
-                    intervenant += line
-            location = event.location.encode('latin-1').decode('utf-8')
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
-        else:
-            return "Aucun événement trouvé pour cette date l'aprem."
-        
-
-    elif commande == "demain":
-        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-        calendar = Calendar(response.text)
-        events = [event for event in calendar.events if event.begin.date() == tomorrow]
-        if events:
-            # pour une raison que je ne comprends pas, ici, le seul moyen d'afficher le premier événement de la liste est de prendre le deuxième
-            event = events[1]
-            description = event.description.encode('latin-1').decode('utf-8')
-            intervenant = ""
-            for line in description.splitlines():
-                if line.startswith("- Intervenant(s) :"):
-                    intervenant += line
-            location = event.location.encode('latin-1').decode('utf-8')
-            return f"Salle : {location}Commence à : {event.begin.time()}\n{intervenant[2:]}"
-        else:
-            return "Aucun événement trouvé pour demain."
-        
-    else:
-        return "Wut?"
-
 def get_the_comande(id):
     return f"id du salon update! \n Prochain message automatique a {HEURE} "
 #################################################
 
-# Action à effectuer pour la commande !matin
-@bot.command(name='matin', help="Affiche la salle de classe pour ce matin")
-async def room_morning_command(ctx):    
-    info = get_the_right_room("matin")
-    await ctx.send(info)
-
-
-@bot.command(name='aprem', help="Affiche la salle de classe pour cet après-midi")
-async def room_afternoon_command(ctx):
-    info = get_the_right_room("aprem")
-    await ctx.send(info)
-
-
-@bot.command(name='demain', help="Affiche la salle de classe pour le lendemain")
-async def room_tomorrow_command(ctx):    
-    info = get_the_right_room("demain")
-    await ctx.send(info)
     
 @bot.command(name='id', help="Affiche la salle de classe pour le lendemain")
 async def command(ctx):    
@@ -113,11 +36,6 @@ async def command(ctx):
     await ctx.send(info)
     await on_ready()
 
-@bot.command(name='semaine', help="Affiche la salle de classe pour ce matin")
-async def room_afternoon_command(ctx):
-    info = semaine()
-    await ctx.send(info)
-    
 def automatique():
     response = requests.get(ICS_URL)
     today = datetime.date.today()
@@ -167,15 +85,6 @@ async def send_message():
     if channel:
         await channel.send(automatique())
 
-"""
-def semaine():
-    print("tenetative de faire une semaine")
-    dt = datetime.now()
-    # get day of the week as an integer (Monday = 0, Sunday = 6)
-    jour_nombre = dt.weekday()
-    for i in range(0,6): #faire afichr la semaine actuelle  
-    return jour_semaine
-"""   
 
 print('Bot is ready.')
 print('heure prevu',HEURE)
