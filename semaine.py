40d0d50a59d12c840463feb641f5e6257e310cfa
redhox <morgan.c@neuf.fr>
2023-07-24 15:29:59 +0200

    ajout du csv et du chois

--- semaine.py
+++ semaine.py
@@ -15,25 +15,27 @@ def semaine():
     # joure de la semaine (lundi = 0, dimanche = 6)
     joure_actuel = datetime.datetime.now().weekday()
     #print("jour=",joure_actuel)
-    semaine_resultat = []
+    semaine_resultat = [[],[]]
+    liste_joure=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+    y=0
     i=joure_actuel 
     while i >= 0:
-        semaine_resultat.append(i*(-1))
+        semaine_resultat[0].append(i*(-1))
+        semaine_resultat[1].append(liste_joure[y])
         i=i-1
+        y=y+1
     i=1
     while i <= 6-joure_actuel:
-        semaine_resultat.append(i)
+        semaine_resultat[0].append(i)
+        semaine_resultat[1].append(liste_joure[y])
         i=i+1
+        y=y+1
+    
     print("semaine =" ,semaine_resultat)
     return semaine_resultat
+rep=st.text_input('le input')
 
-response = requests.get('ICS')
-calendar = Calendar(response.text)
-jour_semaine=semaine()
-
-# date location horaire horaire_fin description     
-
-def data_planning(jour_n):
+def data_planning(jour_n,jour_string):
     date = datetime.date.today() + datetime.timedelta(days=jour_n)
     events = [event for event in calendar.events if event.begin.date() == date]
     if events:
@@ -45,13 +47,30 @@ def data_planning(jour_n):
             location = i.location.encode('latin-1').decode('utf-8') if i.location is not None else ""
             horaire = i.begin.time().strftime("%H:%M")
             horaire_fin = i.end.time().strftime("%H:%M")
-            journee.append([date, location, horaire, horaire_fin, description])
-        return pd.DataFrame(journee, columns=['date', 'location', 'horaire', 'horaire_fin', 'description'])
+            journee.append([jour_string,date, location, horaire, horaire_fin, description])
+        return pd.DataFrame(journee, columns=['jour','date', 'location', 'horaire', 'horaire_fin', 'description'])
+df = pd.DataFrame(columns=['jour','date', 'location', 'horaire', 'horaire_fin', 'description'])
+@st.cache_data 
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
 
-df = pd.DataFrame(columns=['date', 'location', 'horaire', 'horaire_fin', 'description'])
-
-for i in range(len(jour_semaine)):
-    print(jour_semaine[i])
-    df = df._append(data_planning(jour_semaine[i]), ignore_index=True)
+if rep:
+    response = requests.get(rep)
+    calendar = Calendar(response.text)
+    jour_semaine=semaine()
+    for i in range(len(jour_semaine[0])):
+        print(f"jour{jour_semaine[0][i]}:{jour_semaine[1][i]} ")
+        print(f"nombre de jour{len(jour_semaine)}")
+        df = df._append(data_planning(jour_semaine[0][i],jour_semaine[1][i]), ignore_index=True)
+        
+    df.to_csv("./semaine.csv", index=False)
+    st.dataframe(df)
     
-df.to_csv("./semaine.csv", index=False)
+    csv = convert_df(df)
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='semaine.csv',
+        mime='text/csv',
+    )
