e81525dbead13e64ad32d33f6acdc35405ec3c8e
coulm <morgan.coulm@gmail.com>
2023-09-11 23:12:49 +0200

    pdf affichage fait

--- test_semain.py
+++ test_semain.py
@@ -6,9 +6,6 @@ from fpdf import FPDF
 
 import base64
 
-
-
-
 import datetime
 from ics import Calendar
 import requests
@@ -20,11 +17,15 @@ import requests
 from ics import Calendar, Event
 import numpy as np
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv() #load la rariable TOKEN et ICS_URL
+ICS_URL = os.getenv('ICS_URL')
+
 
 def semaine():
     # joure de la semaine (lundi = 0, dimanche = 6)
     joure_actuel = datetime.datetime.now().weekday()
-    #print("jour=",joure_actuel)
     semaine_resultat = [[],[]]
     liste_joure=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
     y=0
@@ -40,8 +41,6 @@ def semaine():
         semaine_resultat[1].append(liste_joure[y])
         i=i+1
         y=y+1
-    
-    print("semaine =" ,semaine_resultat)
     return semaine_resultat
 rep=st.text_input('le input')
 
@@ -67,12 +66,10 @@ def convert_df(df):
         return df.to_csv().encode('utf-8')
 
 
-response = requests.get("url de l'ics")
+response = requests.get("lien ics ici")
 calendar = Calendar(response.text)
 jour_semaine=semaine()
 for i in range(len(jour_semaine[0])):
-    print(f"jour{jour_semaine[0][i]}:{jour_semaine[1][i]} ")
-    print(f"nombre de jour{len(jour_semaine)}")
     df = df._append(data_planning(jour_semaine[0][i],jour_semaine[1][i]), ignore_index=True)
     
 df.to_csv("./semaine.csv", index=False)
@@ -110,17 +107,12 @@ data = {
     "mercredi": [],
     "jeudi": [],
     "vendredi": [],
-    "samedi": [],
-    "dimanche": [],
     # ... Add data for other days of the week
 }
 
 data_combined = {}
 for day in data.keys():
     data_combined[day] = df[df['jour'] == day].to_dict('records')
-
-# Print the combined data dictionary
-print(data_combined)
 
 # Iterate over each day in the data_combined dictionary
 for day, events in data_combined.items():
@@ -145,17 +137,54 @@ def generate_pdf():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Helvetica', '', 12)
+
+    # Set column width and height
+    col_width = 38
+
+    # Calculate the maximum height among all columns
+    max_height = 0
+    for events in data_combined.values():
+        height = len(events) * 4 * 6
+        if height > max_height:
+            max_height = height
+
+    # Set initial position
+    x = 10
+    y = 10
+
     # Iterate over each day in the data_combined dictionary
     for day, events in data_combined.items():
-        pdf.cell(0, 10, day.capitalize(), ln=True)
-        for event in events:
-            pdf.cell(0, 10, f"Date: {event['date']}", ln=True)
-            pdf.cell(0, 10, f"Location: {event['location']}", ln=True)
-            pdf.cell(0, 10, f"Time: {event['horaire']} - {event['horaire_fin']}", ln=True)
-            pdf.cell(0, 10, f"Description: {event['description']}", ln=True)
+        y=40
+        # Set the fill color for the day header
+        pdf.set_fill_color(200, 220, 255)
+        pdf.set_font('Helvetica', '', 12)
+        # Draw the day header
+        pdf.set_xy(x, y)
+        pdf.cell(col_width, -20, day.capitalize(), 1, 0, 'C', fill=True)
 
-    # Save the PDF file
-    pdf.output("weekly_agenda.pdf")
+        # Iterate over each event for the current day
+        for event in events:
+            # Draw the event details in a column
+            pdf.set_font('Helvetica', '', 10)
+            pdf.set_xy(x, y)
+            pdf.multi_cell(col_width, 6, f"Date: {event['date']}", 1)
+            pdf.set_xy(x, y + 6)
+            pdf.multi_cell(col_width, 6, f"Salle: {event['location']}", 1)
+            pdf.set_xy(x, y + 12)
+            pdf.multi_cell(col_width, 6, f"{event['horaire']} - {event['horaire_fin']}", 1)
+            pdf.set_xy(x, y + 18)
+            pdf.set_font('Helvetica', '', 8)
+            pdf.multi_cell(col_width, 4, f"{event['description']}", 1)
+            pdf.set_font('Helvetica', '', 10)
+
+            y += 80
+
+            
+
+        # Move to the next column
+        x += col_width
+
+    pdf.output('agenda.pdf')
     
     st.download_button(
         "Download Report",
